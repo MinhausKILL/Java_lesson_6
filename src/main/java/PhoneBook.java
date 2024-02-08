2c035e65c28500f706cc57856f409bb4395ace11
MinhausKILL <warlordsxp@yandex.ru>
2024-02-08 21:50:03 +0300

    Changed the code. Simplified it.

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,50 +1,34 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class PhoneBook {
 
     public static void main(String[] args) {
-        HashMap<String, String> phoneBook = new HashMap<>();
+        Map<String, Set<String>> phoneBook = new HashMap<>();
 
-        // Добавление записей в телефонную книгу (имя, телефон)
-        phoneBook.put("Иванов", "1234567890");
-        phoneBook.put("Сычева", "9876543210");
-        phoneBook.put("Христофоров", "1211222333");
-        phoneBook.put("Малык", "5556667772");
-        phoneBook.put("Малык", "5545507666");
+        // Добавляем данные в телефонную книгу
+        addContact(phoneBook, "Иванов", "1234567890");
+        addContact(phoneBook, "Малык", "9876543210");
+        addContact(phoneBook, "Сычева", "2468101214");
+        addContact(phoneBook, "Христофоров", "5555552345");
+        addContact(phoneBook, "Малык", "9655507666");
 
-        // Создание новой Map для хранения имени и списка его телефонов
-        Map<String, String> consolidatedPhoneBook = new HashMap<>();
-
-        // Объединение записей с одинаковыми именами
-        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-            String name = entry.getKey();
-            String phone = entry.getValue();
-
-            if (consolidatedPhoneBook.containsKey(name)) {
-                // Если имя уже есть в consolidatedPhoneBook, добавляем телефон к существующему списку телефонов
-                String existingPhones = consolidatedPhoneBook.get(name);
-                consolidatedPhoneBook.put(name, existingPhones + ", " + phone);
-            } else {
-                // Иначе создаем новую запись
-                consolidatedPhoneBook.put(name, phone);
+        // Сортируем по убыванию числа телефонов
+        List<Map.Entry<String, Set<String>>> sortedContacts = new ArrayList<>(phoneBook.entrySet());
+        Collections.sort(sortedContacts, new Comparator<Map.Entry<String, Set<String>>>() {
+            @Override
+            public int compare(Map.Entry<String, Set<String>> entry1, Map.Entry<String, Set<String>> entry2) {
+                return entry2.getValue().size() - entry1.getValue().size();
             }
-        }
+        });
 
-        // Создаем TreeMap для сортировки по убыванию числа телефонов
-        TreeMap<Integer, String> sortedPhoneBook = new TreeMap<>((p1, p2) -> p2 - p1);
-
-        // Перекидываем записи из consolidatedPhoneBook в sortedPhoneBook с учетом количества телефонов
-        for (Map.Entry<String, String> entry : consolidatedPhoneBook.entrySet()) {
-            String name = entry.getKey();
-            int numPhones = entry.getValue().split(", ").length;
-            sortedPhoneBook.put(numPhones, name + ": " + entry.getValue());
+        // Выводим данные
+        for (Map.Entry<String, Set<String>> entry : sortedContacts) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
+    }
 
-        // Выводим отсортированный результат
-        for (Map.Entry<Integer, String> entry : sortedPhoneBook.entrySet()) {
-            System.out.println(entry.getValue());
-        }
+    public static void addContact(Map<String, Set<String>> phoneBook, String name, String phone) {
+        phoneBook.putIfAbsent(name, new HashSet<>());
+        phoneBook.get(name).add(phone);
     }
 }
